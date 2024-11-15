9b2e2a1999ad139f67d21c0db692f3bee66d3fa0
TEAM-S-WEB <sabah.family.1111@gmail.com>
2024-11-15 15:54:02 +0200

    change the blog page, i take all the jsx to a new component

--- src/app/blogs/[...id]/page.jsx
+++ src/app/blogs/[...id]/page.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { Metadata } from "next";
 import { VT323 } from "next/font/google";
-import Image from "next/image";
-import Link from "next/link";
+
 import GlobalApi from "@/utils/GlobalApi";
 import styles from "./blogpage.module.css";
-import NetworkNavbar from "@/components/common/Networks/NetworkNavbar";
+import BlogsComponent from "@/components/blogsComponent/BlogsComponent";
 
 const dateFont = VT323({ weight: "400", subsets: ["latin"] });
 
@@ -47,43 +46,7 @@ const page = async ({ params }) => {
   if (!blog) {
     console.log(" requst");
   }
-  return (
-    <div className={styles.container}>
-      <header>
-        <h1>YLS SERVICES</h1>
-      </header>
-      <div className={styles.imageContainer}>
-        <Image
-          fill
-          className={styles.image}
-          src={blog.pic.url}
-          alt="/blogimage"
-        />
-      </div>
-      <div className={styles.textContainer}>
-        <h1 style={{ margin: "40px 0", fontWeight: 100 }}>{blog?.title}</h1>
-        <span style={{ color: "gray", padding: "3px" }}>
-          <p>DATE: {blog.date}</p>
-        </span>
-        <div className={styles.paragraphContainer}>
-          <p>{blog?.article}</p>
-        </div>
-        <div style={{ marginTop: "15px", marginBottom: "5px" }}>
-          <h2 className={styles.subtitle}>{blog?.subtitle}</h2>
-        </div>
-        <div className={styles.paragraphContainer}>
-          <p>{blog?.subparagraph}</p>
-        </div>
-        <div style={{ marginTop: "25px" }}>
-          <h2 className={styles.subtitle}>{blog?.titleParagraph}</h2>
-        </div>
-        <div className={styles.paragraphContainer}>
-          <p>{blog?.paragraph}</p>
-        </div>
-        <NetworkNavbar callUrl={"tel:+13472011312"}/>
-      </div>
-    </div>
-  );
+  return <BlogsComponent blog={blog} />;
 };
 
 export default page;
