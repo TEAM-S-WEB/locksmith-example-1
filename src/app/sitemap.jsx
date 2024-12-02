cf1e7758a450a753ef4721c2b3e01354260bcf19
TEAM-S-WEB <sabah.family.1111@gmail.com>
2024-12-02 19:59:12 +0200

    fix the seo

--- src/app/sitemap.jsx
+++ src/app/sitemap.jsx
@@ -5,7 +5,7 @@ export default async function sitemap() {
     async function getBlog() {
         const blogs = await GlobalApi.GetAllBlogs();
         // const blog = blogs?.filter((item) => item.blogId == id);
- 
+
         return blogs;
       }
 
@@ -24,12 +24,6 @@ export default async function sitemap() {
         lastModified: new Date(),
         changeFrequency: 'yearly',
         priority: 1,
-      },
-      {
-        url: 'https://www.locksmithysl.com/blogs',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
       },
     ...blogsUrls,
     ]
