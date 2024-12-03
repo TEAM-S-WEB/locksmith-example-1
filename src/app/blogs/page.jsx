234235843544d2f5873456f5087e2c60c777b011
TEAM-S-WEB <sabah.family.1111@gmail.com>
2024-12-03 23:52:10 +0200

    blogs page

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -3,28 +3,28 @@ import styles from "./blogspage.module.css";
 import BlogsPageComponent from "@/components/blogsComponent/BlogsPageComponent";
 import GlobalApi from "@/utils/GlobalApi";
 
-//  async function getAllBlogs() {
-//   const blogs = await GlobalApi.GetAllBlogs();
-//   if (!blogs) {
-//     return {
-//       notFound: true,
-//     }
-//   }
-//   return blogs;
-// }
-
-
-export async function getStaticProps() {
-  const data = await  GlobalApi.GetAllBlogs();
-  if (!data) {
+ async function getAllBlogs() {
+  const blogs = await GlobalApi.GetAllBlogs();
+  if (!blogs) {
     return {
       notFound: true,
     }
   }
-  return {
-    props: { data },
-  }
+  return blogs;
 }
+
+
+// export async function getStaticProps() {
+//   const data = await  GlobalApi.GetAllBlogs();
+//   if (!data) {
+//     return {
+//       notFound: true,
+//     }
+//   }
+//   return {
+//     props: { data },
+//   }
+// }
 
 
 export const revalidate = 60;
@@ -44,8 +44,8 @@ export const metadata = {
   },
 };
 
-const page = async ({data}) => {
-    const blogs = await data;
+const page = async () => {
+    const blogs = await getAllBlogs();
     if (!blogs) {
       return(
         <div className={styles.container}>
